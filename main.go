b6b7b9154306af665aaa6466102cf277c829ce62
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-12-17 16:42:13 +0530

    handling constants and variables

--- main.go
+++ main.go
@@ -3,7 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world on new line!")
-	fmt.Print("Hello world!")
-	fmt.Print("Hello world! on same line!")
+	// fmt.Println("Hello world on new line!")
+	// fmt.Print("Hello world!")
+	// fmt.Print("Hello world! on same line!")
+
+	const conferenceName = "Go conference"
+	var conferenceTickets = 50 
+	var remainingTickets = 50
+	// fmt.Print("Welcome to our conference ticket booking application!")
+	fmt.Print("Welcome to ", conferenceName, " Ticket Booking Application!")
+	fmt.Print("Get your tickets here to attend!")
+
+	fmt.Print("We have a total of ", conferenceTickets, " tickets avaialiable and ", remainingTickets, " tickets are remaining unsold")
+
+
+
+	
 }
